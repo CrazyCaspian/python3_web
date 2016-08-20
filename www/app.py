9cea5e850471437d13a34d15aaa792df0de2f041
CrazyCaspian <renjunsong0@live.com>
2016-08-20 22:19:09 +0800

    edition_1

--- www/app.py
+++ www/app.py
@@ -5,16 +5,16 @@ from datetime import datetime
 from aiohttp import web
 
 def index(request):
-    return web.Response(body = b'<h1>Awesom</h>')
+    return web.Response(body = b'<h1>Awesom</h>',headers={'Content-Type':'text/html;charset=utf-8'})
 
 @asyncio.coroutine
 def init(loop):
     app = web.Application(loop=loop)
     app.router.add_route('GET','/',index)
-    srv = yield from loop.creat_server(app.make_handler(),'127.0.0.1',9000)
+    srv = yield from loop.create_server(app.make_handler(),'127.0.0.1', 9000)
     logging.info('server started at http://127.0.0.1:9000...')
     return srv
 
 loop = asyncio.get_event_loop()
-loop.run_unitl_complete(init(loop))
+loop.run_until_complete(init(loop))
 loop.run_forever
